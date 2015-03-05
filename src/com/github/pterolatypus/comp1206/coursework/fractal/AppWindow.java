015f0f8c861e1645f03993ad090550e4a2327217
Pterolatypus <pterolatypus@gmail.com>
2015-03-05 15:09:45 +0000

    Fixed DraggablePanel

--- src/com/github/pterolatypus/comp1206/coursework/fractal/AppWindow.java
+++ src/com/github/pterolatypus/comp1206/coursework/fractal/AppWindow.java
@@ -1,10 +1,15 @@
 package com.github.pterolatypus.comp1206.coursework.fractal;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class AppWindow extends JFrame {
 
-	private static final long serialVersionUID = 3254183707783343297L;
+	private static final long serialVersionUID = 0L;
 
 	public AppWindow() {
 		super("Fract: Interactive Fractal Visualizer");
@@ -12,9 +17,18 @@ public class AppWindow extends JFrame {
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 		setBounds(0,0,512,512);
-		//this.setLayout(new BorderLayout());
-		
-		this.add(new DraggablePanel());
+		this.setLayout(new BorderLayout());
+
+		JPanel pnlCenter = new JPanel();
+		DraggablePanel pnlDragTest = new DraggablePanel(pnlCenter);
+		pnlDragTest.pSetPreferredSize(new Dimension(64, 64));
+		pnlDragTest.setBackground(Color.RED);
+		pnlCenter.add(pnlDragTest);
+		this.add(pnlCenter, BorderLayout.CENTER);
+	}
+	
+	public static long getSVUID() {
+		return serialVersionUID;
 	}
 	
 }
--- src/com/github/pterolatypus/comp1206/coursework/fractal/DraggablePanel.java
+++ src/com/github/pterolatypus/comp1206/coursework/fractal/DraggablePanel.java
@@ -1,7 +1,10 @@
 package com.github.pterolatypus.comp1206.coursework.fractal;
 
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -9,27 +12,84 @@ import javax.swing.JPanel;
 
 public class DraggablePanel extends JPanel {
 
-	private static final long serialVersionUID = 8100130353703535801L;
+	private static final long serialVersionUID = AppWindow.getSVUID();
 
-	private Point relative;
-	
-	public DraggablePanel() {
+	// Defines a pixel border around the inside of the parent container where
+	// this panel cannot be dragged
+	private int borderOffset = 5;
+
+	// Defines the distance within which the component must be to the edge of
+	// the parent container to snap to the edge
+	// If snapDistance <= borderOffset no snapping occurs
+	private int snapDistance = 15;
+
+	public DraggablePanel(final Container parent) {
+
 		super();
-		setBackground(Color.RED);
-		this.addMouseMotionListener(new MouseAdapter() {
+
+		// The listener which will handle the dragging
+		MouseAdapter m = new MouseAdapter() {
+
+			// Where the mouse was clicked relative to the component
+			private Point relative;
+
 			@Override
 			public void mouseDragged(MouseEvent e) {
-				int newx = (int)(e.getX()-0);
-				int newy = (int)(e.getY()-0);
-				setBounds(newx,newy,getWidth(), getHeight());
+				int newx = (int) (getX() + e.getX() - relative.getX());
+				int newy = (int) (getY() + e.getY() - relative.getY());
+				Point target = getInBounds(new Point(newx, newy),
+						new Rectangle(parent.getWidth(), parent.getHeight()));
+				setBounds((int) target.getX(), (int) target.getY(), getWidth(),
+						getHeight());
 			}
-		});
-		this.addMouseListener(new MouseAdapter(){
+
 			@Override
 			public void mousePressed(MouseEvent e) {
 				relative = e.getPoint();
 			}
-		});
+		};
+
+		this.addMouseMotionListener(m);
+		this.addMouseListener(m);
 	}
-	
+
+	public Point getInBounds(Point corner, Rectangle bounds) {
+
+		// The point we want to move to
+		int newX = (int) corner.getX();
+		int newY = (int) corner.getY();
+
+		// The maximum x and y the corner can be at within the bounds
+		int widthBound = (int) (bounds.getWidth() - getWidth() - borderOffset);
+		int heightBound = (int) (bounds.getHeight() - getHeight() - borderOffset);
+
+		newX = (newX < borderOffset + snapDistance) ? borderOffset : newX;
+		newX = (newX > widthBound - snapDistance) ? widthBound : newX;
+
+		newY = (newY < borderOffset + snapDistance) ? borderOffset : newY;
+		newY = (newY > heightBound - snapDistance) ? heightBound : newY;
+
+		return new Point(newX, newY);
+	}
+
+	public DraggablePanel pSetBorderOffset(int newBorderOffset) {
+		this.borderOffset = newBorderOffset;
+		return this;
+	}
+
+	public DraggablePanel pSetSnapDistance(int newSnapDistance) {
+		this.snapDistance = newSnapDistance;
+		return this;
+	}
+
+	public DraggablePanel pSetBounds(int x, int y, int width, int height) {
+		setBounds(x, y, width, height);
+		return this;
+	}
+
+	public DraggablePanel pSetPreferredSize(Dimension size) {
+		setPreferredSize(size);
+		return this;
+	}
+
 }
